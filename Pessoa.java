ec9bad73e6eff98764270c02a9726e9d32bf0853
Robson Alecio Somera <robson.alecio@gmail.com>
2017-02-17 21:49:38 -0200

    Adicionando informação de saldo na conta

--- Pessoa.java
+++ Pessoa.java
@@ -1,10 +1,12 @@
 public class Pessoa {
   private String nome;
   private int idade;
+  private double saldoNaConta;
 
-  public Pessoa (String nome, int idade) {
+  public Pessoa (String nome, int idade, double saldoNaConta) {
     this.nome = nome;
     this.idade = idade;
+    this.saldoNaConta = saldoNaConta;
   }
 
   public String getNome() {
@@ -13,6 +15,10 @@ public class Pessoa {
 
   public int getIdade() {
     return idade;
+  }
+
+  public double getSaldoNaConta() {
+    return saldoNaConta;
   }
 
 }
